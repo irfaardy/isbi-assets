2158bfda10f321fecccda8c55f3852ea798ba000
Irfa Ardiansyah <irfardy2@gmail.com>
2022-08-10 13:24:16 +0700

    FIX SIDEBAR

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,5 +1,5 @@
 <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <ul class="nav nav-pills nav-sidebar nav-legacy nav-flat flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
@@ -23,43 +23,82 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{route('data.aset.masuk')}}" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Aset Masuk</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('data.aset.keluar')}}" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Aset Keluar</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('data.aset')}}" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data Aset</p>
-                </a>
-              </li> 
-                 @if(auth()->user()->role == 'admin')
-              <li class="nav-item">
-                <a href="{{route('pengguna')}}" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data User</p>
-                </a>
-              </li> 
-              <li class="nav-item">
-                <a href="{{route('kategori.aset')}}" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data Kategori</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('jenis.aset')}}" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data Jenis</p>
-                </a>
-              </li>
-              @endif
+                  <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-database"></i>
+                    <p>
+                       Data Aset
+                      <i class="right fas fa-angle-left"></i>
+                    </p>
+                  </a>
+                  <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                      <a href="{{route('data.aset.masuk')}}" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Aset Masuk</p>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="{{route('data.aset.keluar')}}" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Aset Keluar</p>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="{{route('data.aset')}}" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Data Aset</p>
+                      </a>
+                    </li> 
+                       @if(auth()->user()->role == 'admin')
+                    <li class="nav-item">
+                      <a href="{{route('pengguna')}}" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Data User</p>
+                      </a>
+                    </li> 
+                  
+                    @endif  
+
+                   
+                  </ul>
+
+                    @if(auth()->user()->role == 'admin')
+                            <li class="nav-item">
+                              <a href="{{route('pengguna')}}" class="nav-link">
+                                <i class="far fa-user nav-icon"></i>
+                                <p>Data User</p>
+                              </a>
+                            </li> 
+                          
+                    @endif 
+                    @if(auth()->user()->role == 'admin')
+                    <li class="nav-item">
+                          <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-server"></i>
+                            <p>
+                               Data Lainnya
+                              <i class="right fas fa-angle-left"></i>
+                            </p>
+                          </a>
+                          <ul class="nav nav-treeview">
+                           
+
+                             
+                              <li class="nav-item">
+                              <a href="{{route('kategori.aset')}}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Data Kategori</p>
+                              </a>
+                            </li>
+                            <li class="nav-item">
+                              <a href="{{route('jenis.aset')}}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Data Jenis</p>
+                              </a>
+                            </li>
+                          
+                          </ul>
+                            @endif
             </ul>
           </li> 
           @endif
